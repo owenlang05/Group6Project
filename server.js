097377d9547344ee2184ec104120bc7e5373fcd4
adrummer1 <andrew@bluestemcreativeco.com>
2023-09-28 19:59:47 -0500

    Changed force back to false

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(require('./controllers'));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
 
